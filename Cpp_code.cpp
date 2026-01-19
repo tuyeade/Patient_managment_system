6600c793795cc4473409b68bd40bda7fdd2b2b58
Trufat <trufatadmasu@gmail.com>
2026-01-19 16:42:30 +0300

    Implement patient update functionality
    Added a function to update patient details in the binary file.

--- Cpp_code.cpp
+++ Cpp_code.cpp
@@ -240,3 +240,43 @@ void searchPatient() {
 
     file.close();
 }
+// ---------------- UPDATE PATIENT ----------------
+void updatePatient() {
+    Patient p;
+    int id;
+    bool found = false;
+
+    fstream file("patients.dat", ios::binary | ios::in | ios::out);
+
+    cout << "Enter Patient ID to update: ";
+    cin >> id;
+    cin.ignore();
+
+    while (file.read((char*)&p, sizeof(p))) {
+        if (p.id == id) {
+            cout << "New Name: ";
+            cin.getline(p.name, 50);
+
+            cout << "New Age: ";
+            cin >> p.age;
+
+            cout << "New Gender: ";
+            cin >> p.gender;
+            cin.ignore();
+
+            cout << "New Disease: ";
+            cin.getline(p.disease, 50);
+
+            file.seekp(-sizeof(p), ios::cur);
+            file.write((char*)&p, sizeof(p));
+            found = true;
+            break;
+        }
+    }
+
+    cout << (found ? "Updated successfully!\n" : "Patient not found!\n");
+    file.close();
+}
+
+
+
