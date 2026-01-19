e32e799d809017991a9f1ac100f843eced0d8944
Trufat <trufatadmasu@gmail.com>
2026-01-19 12:31:17 +0300

    Update Signup.cpp

--- Signup.cpp
+++ Signup.cpp
@@ -1,1 +1,16 @@
-//signup
+void signUp() {
+    User u;
+    ofstream file("users.dat", ios::binary | ios::app);
+
+    cout << "\n--- SIGN UP ---\n";
+    cout << "Create Username: ";
+    cin >> u.username;
+
+    cout << "Create Password: ";
+    cin >> u.password;
+
+    file.write((char*)&u, sizeof(u));
+    file.close();
+
+    cout << "Sign up successful! You can now log in.\n";
+}
