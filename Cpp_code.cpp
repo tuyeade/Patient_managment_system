8577dc9a6970eeb4fcb28d354aa81c41922e90ba
yayneabeba-emshaw <yayneabeba98@gmail.com>
2026-01-19 16:27:04 +0300

    2.view patient

--- Cpp_code.cpp
+++ Cpp_code.cpp
@@ -183,3 +183,33 @@ void addPatient(Patient *p) {
 
     cout << "Patient added successfully!\n";
 }
+// ---------------- VIEW PATIENTS ----------------
+void viewPatients() {
+    Patient p;
+    ifstream file("patients.dat", ios::binary);
+
+    if (!file) {
+        cout << "No patient records found.\n";
+        return;
+    }
+
+    cout << left
+         << setw(6)  << "ID"
+         << setw(20) << "Name"
+         << setw(6)  << "Age"
+         << setw(10) << "Gender"
+         << setw(20) << "Disease" << endl;
+
+    cout << "------------------------------------------------------------\n";
+
+    while (file.read((char*)&p, sizeof(p))) {
+        cout << left
+             << setw(6)  << p.id
+             << setw(20) << p.name
+             << setw(6)  << p.age
+             << setw(10) << p.gender
+             << setw(20) << p.disease << endl;
+    }
+
+    file.close();
+}
